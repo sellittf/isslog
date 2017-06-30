dbe363ae03e68852c7a79b47fbee74a8e61d432d
Franco Sellitto <franco.sellitto@zhdk.ch>
2017-06-30 02:29:26 +0200

    typo

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,7 @@
 class LogsController < ApplicationController
 
   def index
-    file = File.join(Rails.root, 'data', 'iss_logs', 'IISLog.log')
+    file = File.join(Rails.root, 'data', 'iis_logs', 'IISLog.log')
     @parsed_log = IisLog.parse file
   end
 
